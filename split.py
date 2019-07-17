c81d9a66c3106912fcf1d534f35b4264432d60a6
stevexnicholls <stevexnicholls@users.noreply.github.com>
2019-07-17 13:30:13 -0400

    Release v0.1.0 (#2)
    * updating gitignore
    
    * updating plugin based on ansible devel docs
    
    * handle empty string in split_regex

--- split.py
+++ split.py
@@ -1,17 +1,38 @@
-from ansible import errors
-import re
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import AnsibleError, AnsibleFilterError
+
+try:
+    import re
+    HAS_LIB = True
+except ImportError:
+    HAS_LIB = False
 
 def split_string(string, separator=' '):
+    '''Split string into a list using delimiter. Example:
+    - debug: msg="{{ csv | split(",") }}"
+    '''
+    if not string:
+      return None
+
     try:
         return string.split(separator)
     except Exception, e:
-        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % str(e),str(string) )
+        raise AnsibleFilterError('split filter plugin error: %s, provided string: "%s"' % str(e),str(data) )
 
 def split_regex(string, separator_pattern='\s+'):
+
+    if not HAS_LIB:
+      raise AnsibleError('Python RegEx Module not available. ')
+
+    if not string:
+      return None
+                   
     try:
         return re.split(separator_pattern, string)
     except Exception, e:
-        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % str(e),str(string) )
+        raise AnsibleFilterError('split filter plugin error: %s, provided string: "%s"' % str(e),str(string) )
 
 class FilterModule(object):
     ''' A filter to split a string into a list. '''
